6d438443f289dbbe8fa6b9f5a06240835d9e1ea9
LuisJerezz <luisjerezz04@gmail.com>
2025-02-19 10:38:37 +0100

    first commit

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,22 @@
+package com.example
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("/prueba") {
+            call.respondText("prueba")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        staticResources("/static", "static")
+    }
+}
